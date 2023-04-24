d7975d4e187edfaac5606f832913715a4d185fcc
Morgan <morganokeke@gmail.com>
2023-04-24 02:59:45 -0700

    using Puppet to make changes to our configuration file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#using Puppet to make changes to our configuration file
+file { '/etc/ssh/ssh_config':
+  ensure => present,
+}->
+file_line { 'To disable password login':
+  path => '~/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+}
+file_line { 'to add path to find the keys':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
+}
