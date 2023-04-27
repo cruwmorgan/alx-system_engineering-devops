5ef38e07df7abd373752cee6fa3af29b5fd8001d
Morgan <morganokeke@gmail.com>
2023-04-27 07:42:31 -0700

    Install Nginx web server (w/ Puppet)

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,14 @@
+#config server using puppet
+
+package { 'nginx':
+  provider => 'apt',
+}
+exec {'edit_page':
+  command => '/usr/bin/sudo /bin/echo Hello World! > /var/www/html/index.nginx-debian.html',
+}
+exec {'redirect_page':
+  command => '/usr/bin/sudo /bin/sed -i "66i rewrite ^/redirect_me https://www.youtube.com/ permanent;" /etc/nginx/sites-available/default',
+}
+exec {'start_server':
+  command => '/usr/bin/sudo /usr/sbin/service nginx start',
+}
