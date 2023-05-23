9dc162b5e227d916e7aac7c63e8dd3b811782e6a
cruwmorgan <morganokeke@gmail.com>
2023-05-23 00:32:45 -0700

    extend your Python script to export data in the CSV format from task1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -18,12 +18,13 @@ if __name__ == "__main__":
     todos_info = rone.json()
 
     with open(per_id_csv, mode='w') as csvfile:
-        my_file = csv.writer(csvfile, delimiter = ',', quotechar='"',
-                              quoting=csv.QUOTE_ALL)
+        my_file = csv.writer(csvfile, delimiter=',', quotechar='"',
+                             quoting=csv.QUOTE_ALL)
         """loops through and finds the needed id"""
         for x in user_info:
             if per_id == x.get("id"):
                 user_name = (x.get("username"))
         for y in todos_info:
             if per_id == y.get("userId"):
-                my_file.writerow([str(per_id), user_name, y.get("completed"), y.get("title")])
+                my_file.writerow([str(per_id), user_name, y.get("completed"),
+                                 y.get("title")])
